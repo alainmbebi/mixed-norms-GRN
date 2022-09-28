081ac4f6aabbb8fed7e40d565a3e18625221b8b3
Alain Mbebi <66583407+alainmbebi@users.noreply.github.com>
2022-09-28 11:54:13 +0200

    Update Dream5_insilico_example_run.R

--- Codes/Dream5_insilico_example_run.R
+++ Codes/Dream5_insilico_example_run.R
@@ -1,15 +1,19 @@
-#This is an example run for L1L21 solution with in silico data (network 1)
+#This is an example run for L1L21 solution with S. cerevisiae data (network 4 from DREAM5 challenge)
 source("Mixed_L1L21_GRN.R")
 
-##### read the expression profile matrix
-expr <- as.matrix(read.table(file = "net1_expression_data.tsv",header = T,sep = "\t",stringsAsFactors = F))
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##### read expression data
+expr <- as.matrix(read.table(file = "net4_expression_data.tsv",header = T,sep = "\t",stringsAsFactors = F))
+head(expr[,1:6])
+dim(expr)
 expr=scale(expr)
 ##### read transcription factors
-TFs <- read.table(file = "net1_transcription_factors.tsv",header = F,sep = "\t",stringsAsFactors = F)
-TFs <- TFs$V1)
+TFs <- read.table(file = "net4_transcription_factors.tsv",header = F,sep = "\t",stringsAsFactors = F)
+TFs <- TFs$V1
+head(TFs)
+length(TFs)
 ##### read Gold standard network
-GS <- read.table(file = "DREAM5_NetworkInference_GoldStandard_Network1_Insilico.tsv",header = F,sep = "\t",stringsAsFactors = F)
-GS <- GS[,c(2,1,3)]
+GS <- read.table(file = "DREAM5_NetworkInference_GoldStandard_Network4 - S. cerevisiae.tsv",header = F,sep = "\t",stringsAsFactors = F)
 colnames(GS) <- c( "from","to",paste("weight","GS",sep = "_"))
 head(GS)
 dim(GS)
@@ -25,19 +29,9 @@ ordernet <- function(net,TFs)
   net[wrong_orders,1] <- net[wrong_orders,2]
   net[wrong_orders,2] <- tmp
   
-  ############## (5) Merge duplicates and remove duplicates and self interactions
-  # sort based on from and to columns
   net <- net[order(net$from,net$to),]
-  
-  # remove self interactions
   net <- net[which(net$from!=net$to),]
-  
-  print(dim(net))
-  
   net <- net[!duplicated(net[,1:2]),]
-  
-  print(dim(net))
-  
   return(net)
 }
 GS <- ordernet(GS,TFs)
@@ -47,35 +41,34 @@ dim(GS)
 GS_genes <- unique(c(GS$from,GS$to))
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#uncomment if the goal is to learn new interactions
+### If the goal is to learn new interactions, then select expression profiles for all genes
 #TGsexpr <- expr
 #TFsexpr <- expr[,colnames(expr)%in%TFs]
 
-### for inference only: select expression profiles for only GS_genes
+### Otherwise (e.g. comparative analysis) select expression profiles for only GS genes only
 TGsexpr <- expr[,which(colnames(expr)%in%GS_genes)]
 TFsexpr <- TGsexpr[,colnames(TGsexpr)%in%TFs]
 TFs=colnames(TFsexpr)
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-itermax=50
-Yl21=TGsexpr 
-Xl21=TFsexpr
-### remove duplicate colnames (if any) in a matrix Yl21 and Xl21 with
-Yl21 <- Yl21[, !duplicated(colnames(Yl21))]
-Xl21 <- Xl21[, !duplicated(colnames(Xl21))]
-##lambdaO=2^(-6) and lambdaB=.6 reproduce the results in the manuscript and they are learned from CV
-ttt=Mixedl1l21(Xl21, Yl21, lambdaO=2^(-6), lambdaB=.6) 
-#Get the regression
-Bhat_l1l21_D5_Insilico=ttt$Bhat_l1l21
-rownames(Bhat_l1l21_D5_Insilico) <- colnames(Xl21)
-colnames(Bhat_l1l21_D5_Insilico) <- colnames(Yl21)
-#Get the precision matrix
-Omegahat_l1l21_D5_Insilico=ttt$Omegahat_l1l21
-rownames(Omegahat_l1l21_D5_Insilico) <- colnames(Yl21)
-colnames(Omegahat_l1l21_D5_Insilico) <- colnames(Yl21)
-#Get the covariance matrix
-Sigmahat_l1l21_D5_Insilico=ttt$Sigmahat_l1l21
-rownames(Sigmahat_l1l21_D5_Insilico) <- colnames(Yl21)
-colnames(Sigmahat_l1l21_D5_Insilico) <- colnames(Yl21)
+itermax=50 
+Y=TGsexpr #the response matrix 
+X=TFsexpr #the predictor matrix 
+#lambdaO=2^(-6) and lambdaB=0.8 are the optimal hyperparameters learned from 10-fold CV. This will also reproduce results in the manuscript.
+ttt=Mixedl1l21(X, Y, lambdaO=2^(-6), lambdaB=0.8) 
+#when all genes are used 
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Bhat_l1l21_D5_Scerevisiae=ttt$Bhat_l1l21
+rownames(Bhat_l1l21_D5_Scerevisiae) <- colnames(X)
+colnames(Bhat_l1l21_D5_Scerevisiae) <- colnames(Y)
+
+Omegahat_l1l21_D5_Scerevisiae=ttt$Omegahat_l1l21
+rownames(Omegahat_l1l21_D5_Scerevisiae) <- colnames(Y)
+colnames(Omegahat_l1l21_D5_Scerevisiae) <- colnames(Y)
+
+Sigmahat_l1l21_D5_Scerevisiae=ttt$Sigmahat_l1l21
+rownames(Sigmahat_l1l21_D5_Scerevisiae) <- colnames(Y)
+colnames(Sigmahat_l1l21_D5_Scerevisiae) <- colnames(Y)
