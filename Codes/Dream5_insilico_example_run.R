1749d3ef6703933a1bf50c2e2229cb9638db8b60
Alain Mbebi <66583407+alainmbebi@users.noreply.github.com>
2022-09-28 11:57:17 +0200

    Update Dream5_insilico_example_run.R

--- Codes/Dream5_insilico_example_run.R
+++ Codes/Dream5_insilico_example_run.R
@@ -4,19 +4,13 @@ source("Mixed_L1L21_GRN.R")
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##### read expression data
 expr <- as.matrix(read.table(file = "net4_expression_data.tsv",header = T,sep = "\t",stringsAsFactors = F))
-head(expr[,1:6])
-dim(expr)
 expr=scale(expr)
 ##### read transcription factors
 TFs <- read.table(file = "net4_transcription_factors.tsv",header = F,sep = "\t",stringsAsFactors = F)
 TFs <- TFs$V1
-head(TFs)
-length(TFs)
 ##### read Gold standard network
 GS <- read.table(file = "DREAM5_NetworkInference_GoldStandard_Network4 - S. cerevisiae.tsv",header = F,sep = "\t",stringsAsFactors = F)
 colnames(GS) <- c( "from","to",paste("weight","GS",sep = "_"))
-head(GS)
-dim(GS)
 ordernet <- function(net,TFs)
 {
   print(dim(net))
@@ -35,9 +29,6 @@ ordernet <- function(net,TFs)
   return(net)
 }
 GS <- ordernet(GS,TFs)
-head(GS)
-dim(GS)
-
 GS_genes <- unique(c(GS$from,GS$to))
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -57,18 +48,18 @@ Y=TGsexpr #the response matrix
 X=TFsexpr #the predictor matrix 
 #lambdaO=2^(-6) and lambdaB=0.8 are the optimal hyperparameters learned from 10-fold CV. This will also reproduce results in the manuscript.
 ttt=Mixedl1l21(X, Y, lambdaO=2^(-6), lambdaB=0.8) 
-#when all genes are used 
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Get the estimated regression coefficient matrix
 Bhat_l1l21_D5_Scerevisiae=ttt$Bhat_l1l21
 rownames(Bhat_l1l21_D5_Scerevisiae) <- colnames(X)
 colnames(Bhat_l1l21_D5_Scerevisiae) <- colnames(Y)
 
+#Get the estimated precision matrix
 Omegahat_l1l21_D5_Scerevisiae=ttt$Omegahat_l1l21
 rownames(Omegahat_l1l21_D5_Scerevisiae) <- colnames(Y)
 colnames(Omegahat_l1l21_D5_Scerevisiae) <- colnames(Y)
 
+#Get the estimated covariance matrix
 Sigmahat_l1l21_D5_Scerevisiae=ttt$Sigmahat_l1l21
 rownames(Sigmahat_l1l21_D5_Scerevisiae) <- colnames(Y)
 colnames(Sigmahat_l1l21_D5_Scerevisiae) <- colnames(Y)
