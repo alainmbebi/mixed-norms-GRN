08239482e028230fd7160fa0daaa670ff8b0aa8e
Alain Mbebi <66583407+alainmbebi@users.noreply.github.com>
2022-12-21 09:30:48 +0100

    Update L1L21_Dream5_Scerevisia_example_run.R

--- Codes/L1L21_Dream5_Scerevisia_example_run.R
+++ Codes/L1L21_Dream5_Scerevisia_example_run.R
@@ -1,4 +1,5 @@
 #This is an example run for L1L21 solution with S. cerevisiae data (network 4 from DREAM5 challenge)
+#the function "ordernet" is adapted from the source code in Fused LASSO (https://github.com/omranian/inference-of-GRN-using-Fused-LASSO#start-of-content)
 source("Mixed_L1L21_GRN.R")
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -58,4 +59,28 @@ colnames(Omegahat_l1l21_D5_Scerevisiae) <- colnames(Y)
 Sigmahat_l1l21_D5_Scerevisiae=ttt$Sigmahat_l1l21
 rownames(Sigmahat_l1l21_D5_Scerevisiae) <- colnames(Y)
 colnames(Sigmahat_l1l21_D5_Scerevisiae) <- colnames(Y)
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Rescale each row of the regression coefficient matrix to range between 0 and 1
+Bhat_l1l21_D5_Scerevisiae=t(apply(abs(Bhat_l1l21_D5_Scerevisiae), 1, function(x)(x-min(x))/(max(x)-min(x))))
+mixedl1l21_D5_Scerevisiae <- reshape2::melt(Bhat_l1l21_D5_Scerevisiae)
+mixedl1l21_D5_Scerevisiae <- mixedl1l21_D5_Scerevisiae[,c(2,1,3)]
+colnames(mixedl1l21_D5_Scerevisiae) <- c("from","to","weight_mixedl1l21_D5_Scerevisiae")
+mixedl1l21_D5_Scerevisiae=rm_NTF_int(mixedl1l21_D5_Scerevisiae,TFs)
+mixedl1l21_D5_Scerevisiae <- ordernet(mixedl1l21_D5_Scerevisiae,TFs)
 
+mixedl1l21_D5_Scerevisiae <- mixedl1l21_D5_Scerevisiae[which(abs(mixedl1l21_D5_Scerevisiae$weight_mixedl1l21_D5_Scerevisiae)>0.00001),]
+mixedl1l21_D5_Scerevisiae$score <- abs(mixedl1l21_D5_Scerevisiae$weight_mixedl1l21_D5_Scerevisiae)/max(abs(mixedl1l21_D5_Scerevisiae$weight_mixedl1l21_D5_Scerevisiae))
+mixedl1l21_D5_Scerevisiae <- mixedl1l21_D5_Scerevisiae[order(mixedl1l21_D5_Scerevisiae$score,decreasing = T),]
+mixedl1l21_D5_Scerevisiae=head(mixedl1l21_D5_Scerevisiae,100000)
+
+colnames(mixedl1l21_D5_Scerevisiae)[3:4] <-  c("weight_mixedl1l21_D5_Scerevisiae","score_mixedl1l21_D5_Scerevisiae")
+write.table(mixedl1l21_D5_Scerevisiae, file = "mixedl1l21_D5_Scerevisiae.txt",row.names = FALSE, col.names = TRUE)
+
+nl1l21_D5_Scerevisiae <- merge(mixedl1l21_D5_Scerevisiae,GS,by = c("from","to"),all.y = T)
+nl1l21_D5_Scerevisiae$weight_mixedl1l21_D5_Scerevisiae[which(is.na(nl1l21_D5_Scerevisiae$weight_mixedl1l21_D5_Scerevisiae)==T)] <- 0
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# get the metrics with precrec package
+library(precrec)
+sscurves_nl1l21_D5_Scerevisiae <- evalmod(scores = nl1l21_D5_Scerevisiae$weight_mixedl1l21_D5_Scerevisiae, labels = GS$weight_GS)
+plot(sscurves_nl1l21_D5_Scerevisiae)
+auc(sscurves_nl1l21_D5_Scerevisiae)
