d7b2774731a558467770683be8577ccef006e1a7
Alain Mbebi <66583407+alainmbebi@users.noreply.github.com>
2023-05-23 14:46:35 +0200

    Update Mixed_L2L21G_GRN.R

--- Codes/Mixed_L2L21G_GRN.R
+++ Codes/Mixed_L2L21G_GRN.R
@@ -6,32 +6,34 @@ set.seed(143)
 library(data.table)
 library(MASS)
 library(corpcor)
-library(Matrix)           
+library(Matrix)           # use Diagonal() when computing kronecker instead of diag()
 library(lattice)
+library(mvtnorm)
 library(glasso)           # This will help to estimate sparse Omega_Mixt using Graphical Lasso
-library(glassoFast)       # Faster alternative to glasso
+library(glassoFast)
 library(RColorBrewer)
 library(matrixcalc)
+library(glmnet)
 library(igraph)
 library(precrec)
 library(reshape2)
+
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# '@functions
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #function for the joint estimation for B and Omega in the mixed l1-l21 model
-Mixedl2l21_Gauss<-function(X, Y, lambdaO, lambdaB, zeta=1e-6){
-  #lambdaO regularization parameter on the precition matrix
-  #lambdaB regularization parameter on the regression coefficient matrix
-  #X is the matrix of predictors (TFs)
-  #Y is the matrix of responses (TGs)
-  #zeta is the treshold value when computing the l21norm to avoid division by zero
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #Sigma_ here is P in the mixed l2l21_Gauss algorithm
+Mixedl2l21_Gauss<-function(X, Y, lambdaO, lambdaB, eps=1e-5, zeta=1e-6, tol.out=1e-6){
+  #eps=1e-5, the algorithm will terminate if the minimum diagonal entry of the current iteration for
+  #residual sample covariance is less than eps.
+  #This may need adjustment depending on the scales of the variables.
+  #zeta=1e-8 is the treshold value when computing the l21norm to avoid division by zero
+  #----------------------
+  #***********************************In fact Sigma_ here is P in the mixed l2l21_Gauss algorithm
   Sigma_0=diag(ncol(Y))
   Omega_0=(1/2*lambdaO)*(mpower(Sigma_0^2 + 8*lambdaO*diag(ncol(Y)), 1/2))-(1/2*lambdaO)*Sigma_0
-  P0=t(chol(Sigma_0) + diag(zeta, ncol(Y)))            
+  P0=t(chol(Sigma_0) + diag(zeta, ncol(Y)))               # to get the lower triangular matrix P (notice the transpose otherwise upper triangular)
   
   #----------------------
   SVD_P0=svd(P0)
@@ -66,7 +68,7 @@ Mixedl2l21_Gauss<-function(X, Y, lambdaO, lambdaB, zeta=1e-6){
   #t=1 update
   Sigma_1=round(cov.shrink(Y-X%*%B_0),9)
   Omega_1=(1/2*lambdaO)*(mpower(Sigma_1^2 + 8*lambdaO*diag(ncol(Y)), 1/2))-(1/2*lambdaO)*Sigma_1
-  P1=t(chol(Sigma_1) + diag(zeta, ncol(Y)))               
+  P1=t(chol(Sigma_1) + diag(zeta, ncol(Y)))               # to get the lower triangular matrix P (notice the transpose otherwise upper triangular)
   
   #----------------------
   SVD_P1=svd(P1)
@@ -85,6 +87,7 @@ Mixedl2l21_Gauss<-function(X, Y, lambdaO, lambdaB, zeta=1e-6){
   B_1=V1%*%Btilde1%*%t(U2_1)
   
   #----------------------
+  #compute the l21norm for t(B_0) that will help to update invdiag_C_1
   l21B_1=c(rep(0, ncol(Y)))
   for(i in 1:ncol(Y)){
     l21B_1[i]=2*norm((t(B_1)[i,]),type = "2")
@@ -92,6 +95,7 @@ Mixedl2l21_Gauss<-function(X, Y, lambdaO, lambdaB, zeta=1e-6){
 
   #----------------------
   tcontmixtl2l21_Gauss=0
+
   if((sum(l21B_1)<=sum(l21B_0))==TRUE && tcontmixtl2l21_Gauss<=itermax){
     l21B_0=l21B_1
     Sigma_0=Sigma_1
@@ -112,12 +116,12 @@ Mixedl2l21_Gauss<-function(X, Y, lambdaO, lambdaB, zeta=1e-6){
     #----------------------
     Sigma_1=round(cov.shrink(Y-X%*%B_0),9)
     Omega_1=(1/2*lambdaO)*(mpower(Sigma_1^2 + 8*lambdaO*diag(ncol(Y)), 1/2))-(1/2*lambdaO)*Sigma_1
-    P1=t(chol(Sigma_1) + diag(zeta, ncol(Y)))             
+    P1=t(chol(Sigma_1) + diag(zeta, ncol(Y)))               # to get the lower triangular matrix P (notice the transpose otherwise upper triangular)
     
     #----------------------
     SVD_P1=svd(P1)
     U2_1=SVD_P1$u
-    Psi_1=diag(SVD_P1$d) + diag(zeta, ncol(Y)) 
+    Psi_1=diag(SVD_P1$d) + diag(zeta, ncol(Y)) #sxs
     V2_1=SVD_P1$v
     
     #----------------------
@@ -142,5 +146,56 @@ Mixedl2l21_Gauss<-function(X, Y, lambdaO, lambdaB, zeta=1e-6){
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Mixedl2l21_Gauss cross validation
+CV_Mixedl2l21_Gauss<-function( X, Y, lambdaO.vect, lambdaB.vect, kfold=3){
+  set.seed(123)
+  lambdaO.vect = sort(lambdaO.vect)
+  lambdaB.vect = sort(lambdaB.vect)
+  n = nrow(Y)
+  CV_errors = matrix(0, nrow=length(lambdaO.vect), ncol=length(lambdaB.vect))
+  ind=sample(n)
+  for (k in 1:kfold){
+    
+    leave.out = ind[(1 + floor((k - 1) * n/kfold)):floor(k * n/kfold)]
+    
+    # training set
+    Y.train = Y[-leave.out,]
+    X.train = X[-leave.out,]
+    Y.valid = Y[leave.out, ]
+    X.valid = X[leave.out, ]
 
+    # loop over all tuning parameters
+         for(i in 1:length(lambdaO.vect)){
+         for(j in 1:length(lambdaB.vect)){
+          Estimes.train = Mixedl2l21_Gauss(X.train, Y.train, lambdaO.vect[i], lambdaB.vect[j]) 
+          B_hat.train = Estimes.train[[1]]
+          Omega_hat_train =  Estimes.train[[2]]
+          Sigma_hat_train =  Estimes.train[[3]]
+          CV_errors[i,j] = mean(mse.matrix(X.valid%*%B_hat.train,Y.valid))#eventually use the RV coef
+        }
+       }
+      }
+    # determine optimal tuning parameters
+  AVG = apply(CV_errors, 1, mean)
+  error = min(AVG)
+  opt = which.min(CV_errors) %% (dim(CV_errors)[1])
+  opt = (opt != 0)*opt + (opt == 0)*(dim(CV_errors)[1])
+  opt.i = opt
+  opt.j = which.min(CV_errors[opt,])
+  best.lamO = lambdaO.vect[opt.i]
+  best.lamB = lambdaB.vect[opt.j]
+
+  #output on all data
+  Estimes.all = Mixedl2l21_Gauss(X, Y, best.lamO, best.lamB) 
+  B_hat_final = Estimes.all[[1]]
+  Omega_hat_final =  Estimes.all[[2]]
+  Sigma_hat_final =  Estimes.all[[3]]
+  # return best values in lambdaO.vect, lambdaB.vect and other meaningful values
+  return(list(best.lamO.final=best.lamO, best.lamB.final=best.lamB,
+              B_hat_final=B_hat_final,Omega_hat_final=Omega_hat_final, Sigma_hat_final=Sigma_hat_final, min.error = error, avg.error = AVG, cv.err=CV_errors)) 
+  
+}
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
